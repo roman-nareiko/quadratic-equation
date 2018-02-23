2feb93f98e3c625fc251e7a14b2e9c37fc4b3ee5
Raman Nareika <Raman.Nareika@thomsonreuters.com>
2018-02-23 15:42:55 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  //'479 * x^2 + 301133888 * x - 5544914841268791'
+  let arr = equation.split(" ");
+  let a = parseInt(arr[0]);
+  let b = parseInt(arr[3] + arr[4]);
+  let c = parseInt(arr[7] + arr[8]);
+  let d = Math.pow(b, 2) - 4*a*c;
+  let x1 = Math.round((-b + Math.sqrt(d)) / (2*a));
+  let x2 = Math.round((-b - Math.sqrt(d)) / (2*a));
+
+  return [x1, x2].sort((a, b) => a - b);
 }
